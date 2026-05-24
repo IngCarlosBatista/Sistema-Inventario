849e881b39385f0e9d3d61ffbd6e3b6a70371d12
IngCarlosBatista <ingcarlosbatista@gmail.com>
2026-05-23 21:14:29 -0400

    Creación del archivo principal main.py con estructura base

---
+++ src/main.py
@@ -0,0 +1,8 @@
+def main():
+    print("=========================================")
+    print(" 📦 SISTEMA DE CONTROL DE INVENTARIO UAPA ")
+    print("=========================================")
+    print("¡Conexión exitosa! Inicializando módulos...")
+
+if __name__ == "__main__":
+    main()
